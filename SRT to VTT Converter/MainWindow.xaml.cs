1bbd447529d5d141d6eaf157da0b9498ca6529cf
Nathan Woltman <nwoltman@outlook.com>
2015-03-12 01:06:03 -0700

    Remove the third party element, which means no need for installer
    Also update license year, update comments to my new style, and add post-build event
    to copy the build executable to a "dist" directory.

--- SRT to VTT Converter/MainWindow.xaml.cs
+++ SRT to VTT Converter/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿/*
 The MIT License (MIT)
 
-Copyright (c) 2014 Nathan Woltman
+Copyright (c) 2014-2015 Nathan Woltman
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -19,7 +19,7 @@ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+*/
 
 using System;
 using System.ComponentModel;
@@ -27,6 +27,7 @@ using System.IO;
 using System.Net;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace SRT_to_VTT_Converter
 {
@@ -43,18 +44,18 @@ namespace SRT_to_VTT_Converter
 		/// 1 means add the offset time, -1 means subtract the offset time
 		/// </summary>
 		private int _nOffsetDirection = 1;
-		private long _offsetTicks;
+		private uint _offsetMs;
 
 		public MainWindow()
 		{
 			InitializeComponent();
 
-			//Configure open file dialog box
-			_dlgOpenFile.Filter = "SubRip Subtitles (*.srt)|*.srt"; //Filter files by .srt extension 
+			// Configure open file dialog box
+			_dlgOpenFile.Filter = "SubRip Subtitles (*.srt)|*.srt"; // Filter files by .srt extension 
 			_dlgOpenFile.FileOk += dlgOpenFile_FileOk;
-			_dlgOpenFile.Multiselect = true; //Allow multiple files to be selected
+			_dlgOpenFile.Multiselect = true; // Allow multiple files to be selected
 
-			//Initialize the background worker's info
+			// Configure the background worker
 			_backgroundWorker.WorkerReportsProgress = true;
 			_backgroundWorker.WorkerSupportsCancellation = true;
 			_backgroundWorker.DoWork += BackgroundWorker_DoWork;
@@ -62,66 +63,94 @@ namespace SRT_to_VTT_Converter
 			_backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		// Event handler for when the text of a textbox that only accepts numbers changes
+		private void NumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			var textBox = (TextBox)sender;
+			textBox.Text = Regex.Replace(textBox.Text, @"\D", ""); // Remove all non-digit characters
+		}
+
+		// Event handler for when a textbox that only accepts numbers loses focus
+		private void NumberTextBox_LostFocus(object sender, RoutedEventArgs e)
+		{
+			var textBox = (TextBox)sender;
+			var paddingWidth = textBox.Name == "TbMilliseconds" ? 3 : 2;
+			if (textBox.Text.Length < paddingWidth)
+			{
+				textBox.Text = textBox.Text.PadLeft(paddingWidth, '0');
+			}
+		}
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		private uint getOffsetTime()
+		{
+			var h = UInt32.Parse(TbHours.Text);
+			var m = UInt32.Parse(TbMinutes.Text);
+			var s = UInt32.Parse(TbSeconds.Text);
+			var ms = UInt32.Parse(TbMilliseconds.Text);
+
+			return h * 3600000 + m * 60000 + s * 1000 + ms;
+		}
+
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		private void OpenFile(object sender, RoutedEventArgs e)
 		{
-			//Simply show the open file dialog
+			// Simply show the open file dialog
 			_dlgOpenFile.ShowDialog();
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		//Is called when the open file dialog is closed with a legal file being selected
+		// Is called when the open file dialog is closed with a legal file being selected
 		private void dlgOpenFile_FileOk(object sender, CancelEventArgs e)
 		{
-			//Set up GUI for conversion
-			BtnOpenFile.Visibility = Visibility.Hidden;	//Hide the open file button
-			BtnCancel.Visibility = Visibility.Visible;	//Show the cancel button
-			TpOffset.IsEnabled = false;					//Disable the time picker
-			LblProgress.Content = "Progress: 0%";		//Set displayed progress to 0%
-			TxtOutput.Clear();							//Clear any text in the output textbox
-			TxtOutput.Visibility = Visibility.Visible;	//Show the outptut textbox
+			// Set up GUI for conversion
+			BtnOpenFile.Visibility = Visibility.Collapsed;
+			BtnCancel.Visibility = Visibility.Visible;
+			WpOffsetInput.IsEnabled = false;
+			LblProgress.Content = "Progress: 0%";
+			TxtOutput.Clear();
+			TxtOutput.Visibility = Visibility.Visible;
 
-			//Record the offset before starting the conversion
-			_offsetTicks = TpOffset.Value.HasValue ? TpOffset.Value.Value.TimeOfDay.Ticks : 0;
+			// Record the offset before starting the conversion
+			_offsetMs = getOffsetTime();
 
-			//Run the BackgroundWorker asynchronously to convert the selected files
+			// Run the BackgroundWorker asynchronously to convert the selected files
 			_backgroundWorker.RunWorkerAsync();
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		private void BtnCancel_Click(object sender, RoutedEventArgs e)
 		{
-			//Cancel the BackgroundWorker's process, change the cancel button's content, and disable the cancel button
 			_backgroundWorker.CancelAsync();
 			BtnCancel.Content = "Cancelling...";
 			BtnCancel.IsEnabled = false;
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		private void BtnOffsetPlusMinus_Click(object sender, RoutedEventArgs e)
 		{
-			if (!TpOffset.Value.HasValue)
-				return;
-
-			_nOffsetDirection *= -1; //Toggle the offset direction
-			BtnOffsetPlusMinus.Content = _nOffsetDirection == 1 ? "+" : "-"; //Set the symbol based on the direction
+			_nOffsetDirection *= -1; // Toggle the offset direction
+			BtnOffsetPlusMinus.Content = _nOffsetDirection == 1 ? "+" : "-"; // Set the symbol based on the direction
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		//This event handler is called by running the background worker asynchronously
+		// This event handler is called by running the background worker asynchronously
 		private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
 		{
-			int nConverted = 0; //This will count the number of conversions completed
+			int nConverted = 0; // This will count the number of conversions completed
 
-			//For each of the user's selected files
+			// For each of the user's selected files
 			foreach (var sFile in _dlgOpenFile.FileNames)
 			{
-				//If the user has requested to cancel the conversion, set the Cancel flag and break out of the loop
+				// If the user has requested to cancel the conversion, set the Cancel flag and break out of the loop
 				if (_backgroundWorker.CancellationPending)
 				{
 					e.Cancel = true;
@@ -140,23 +169,23 @@ namespace SRT_to_VTT_Converter
 				}
 				++nConverted;
 				_backgroundWorker.ReportProgress(
-					(int)(nConverted/(double)_dlgOpenFile.FileNames.Length * 100), //% complete
-					nConverted + ". \"" + Path.GetFileName(sFile) + "\" - " + sDoneMsg + "\n" //Done message for the file
+					(int)(nConverted/(double)_dlgOpenFile.FileNames.Length * 100), // % complete
+					nConverted + ". \"" + Path.GetFileName(sFile) + "\" - " + sDoneMsg + "\n" // Done message for the file
 				);
 			}
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-		//This event handler executes on the main thread to update displayed progress in the GUI
+		// This event handler executes on the main thread to update displayed progress in the GUI
 		private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
 		{
-			LblProgress.Content = "Progress: " + e.ProgressPercentage + "%"; //Display percent of progress complete
-			TxtOutput.AppendText((string)e.UserState); //Append the message to the output textbox
-			TxtOutput.ScrollToEnd(); //Scroll to the bottom to keep the most recently converted files visible
+			LblProgress.Content = "Progress: " + e.ProgressPercentage + "%";
+			TxtOutput.AppendText((string)e.UserState); // Append the message to the output textbox
+			TxtOutput.ScrollToEnd(); // Scroll to the bottom to keep the most recently converted files visible
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		//This event handler deals with the results of the background operation
 		private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -164,20 +193,22 @@ namespace SRT_to_VTT_Converter
 			if (e.Cancelled)
 			{
 				TxtOutput.AppendText("PROCESS CANCELLED");
-				BtnCancel.Content = "Cancel"; //Change the cancel button's content back to what it was
-				BtnCancel.IsEnabled = true; //Re-enable the cancel button
+				BtnCancel.Content = "Cancel";
+				BtnCancel.IsEnabled = true;
 			}
 			else if (e.Error != null)
 			{
-				TxtOutput.AppendText("\nERROR!\nThe following error occured during the conversion:\n\n" + e.Error.Message);
+				TxtOutput.AppendText(
+					"\nERROR!\nThe following error occured during the conversion:\n\n" + e.Error.Message
+				);
 			}
 
-			BtnOpenFile.Visibility = Visibility.Visible; //Show the open file button
-			BtnCancel.Visibility = Visibility.Hidden;	 //Hide the cancel button
-			TpOffset.IsEnabled = true;					 //Enable the time picker
+			BtnOpenFile.Visibility = Visibility.Visible;
+			BtnCancel.Visibility = Visibility.Collapsed;
+			WpOffsetInput.IsEnabled = true;
 		}
 
-		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 		private void Convert(string sFilePath)
 		{
@@ -187,50 +218,56 @@ namespace SRT_to_VTT_Converter
 				var rgxDialogNumber = new Regex(@"^\d+$");
 				var rgxTimeFrame = new Regex(@"(\d\d:\d\d:\d\d,\d\d\d) --> (\d\d:\d\d:\d\d,\d\d\d)");
 
-				//Write mandatory starting line for the WebVTT file
+				// Write starting line for the WebVTT file
 				strWriter.WriteLine("WEBVTT");
 				strWriter.WriteLine("");
 
-				//Handle each line of the SRT file
+				// Handle each line of the SRT file
 				string sLine;
 				while ((sLine = strReader.ReadLine()) != null)
 				{
-					//We only care about lines that aren't just an integer (aka ignore dialog id number lines)
+					// We only care about lines that aren't just an integer (aka ignore dialog id number lines)
 					if (rgxDialogNumber.IsMatch(sLine))
 						continue;
 
-					//If the line is a time frame line, reformat and output the time frame
+					// If the line is a time frame line, reformat and output the time frame
 					Match match = rgxTimeFrame.Match(sLine);
 					if (match.Success)
 					{
-						if (_offsetTicks > 0)
+						if (_offsetMs > 0)
 						{
-							//Extract the times from the matched time frame line
+							// Extract the times from the matched time frame line
 							var tsStartTime = TimeSpan.Parse(match.Groups[1].Value.Replace(',', '.'));
 							var tsEndTime = TimeSpan.Parse(match.Groups[2].Value.Replace(',', '.'));
 
-							//Modify the time with the offset, making sure the time span gets set to 0 if it is going to be negative
-							long startTimeTicks = _nOffsetDirection*_offsetTicks + tsStartTime.Ticks;
-							long endTimeTicks = _nOffsetDirection*_offsetTicks + tsEndTime.Ticks;
-							tsStartTime = TimeSpan.FromTicks(startTimeTicks < 0 ? 0 : startTimeTicks);
-							tsEndTime = TimeSpan.FromTicks(endTimeTicks < 0 ? 0 : endTimeTicks);
+							// Modify the time with the offset
+							long startTimeMs = _nOffsetDirection * _offsetMs + (uint)tsStartTime.TotalMilliseconds;
+							long endTimeMs = _nOffsetDirection * _offsetMs + (uint)tsEndTime.TotalMilliseconds;
+							tsStartTime = TimeSpan.FromMilliseconds(startTimeMs < 0 ? 0 : startTimeMs);
+							tsEndTime = TimeSpan.FromMilliseconds(endTimeMs < 0 ? 0 : endTimeMs);
 
-							//Construct the new time frame line
-							sLine = tsStartTime.ToString(@"hh\:mm\:ss\.fff") + " --> " + tsEndTime.ToString(@"hh\:mm\:ss\.fff");
+							// Construct the new time frame line
+							sLine = tsStartTime.ToString(@"hh\:mm\:ss\.fff") +
+									" --> " +
+									tsEndTime.ToString(@"hh\:mm\:ss\.fff");
 						}
 						else
 						{
-							sLine = sLine.Replace(',', '.'); //Simply replace the comma in the time with a period
+							sLine = sLine.Replace(',', '.'); // Simply replace the comma in the time with a period
 						}
 					}
 					else
 					{
-						//HTML-encode the text so it is displayed properly by browsers but then undo unnecessary encodings
+						// HTML-encode the text so it is displayed properly by browsers,
+						// but then undo unnecessary encodings
 						sLine = WebUtility.HtmlEncode(sLine);
-						sLine = sLine.Replace("&#39;", "'").Replace("&quot;", "\"").Replace("&lt;", "<").Replace("&gt;", ">");
+						sLine = sLine.Replace("&#39;", "'")
+						             .Replace("&quot;", "\"")
+						             .Replace("&lt;", "<")
+						             .Replace("&gt;", ">");
 					}
 
-					strWriter.WriteLine(sLine); //Write out the line
+					strWriter.WriteLine(sLine); // Write out the line
 				}
 			}
 		}
