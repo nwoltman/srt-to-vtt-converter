0b8580a69e709da79997f2e5cb6bc24bf344c700
Nathan Woltman <nwoltman@outlook.com>
2018-08-02 15:42:10 -0400

    Reorganize and improve code

--- SRT to VTT Converter/MainWindow.xaml.cs
+++ SRT to VTT Converter/MainWindow.xaml.cs
@@ -38,12 +38,8 @@ namespace SRT_to_VTT_Converter
         private readonly Microsoft.Win32.OpenFileDialog _dlgOpenFile = new Microsoft.Win32.OpenFileDialog();
         private readonly BackgroundWorker _backgroundWorker = new BackgroundWorker();
 
-        /// <summary>
-        /// Always either 1 or -1.
-        /// 1 means add the offset time, -1 means subtract the offset time
-        /// </summary>
-        private int _nOffsetDirection = 1;
-        private uint _offsetMs;
+        private int _offsetMilliseconds = 0;
+        private bool _negativeOffset = false;
 
         public MainWindow()
         {
@@ -51,15 +47,24 @@ namespace SRT_to_VTT_Converter
 
             // Configure open file dialog box
             _dlgOpenFile.Filter = "SubRip Subtitles (*.srt)|*.srt"; // Filter files by .srt extension 
-            _dlgOpenFile.FileOk += dlgOpenFile_FileOk;
+            _dlgOpenFile.FileOk += DlgOpenFile_FileOk;
             _dlgOpenFile.Multiselect = true; // Allow multiple files to be selected
 
             // Configure the background worker
             _backgroundWorker.WorkerReportsProgress = true;
             _backgroundWorker.WorkerSupportsCancellation = true;
             _backgroundWorker.DoWork += BackgroundWorker_DoWork;
-            _backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
             _backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+            _backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // Toggles the timing offset direction
+        private void BtnOffsetPlusMinus_Click(object sender, RoutedEventArgs e)
+        {
+            _negativeOffset = !_negativeOffset;
+            BtnOffsetPlusMinus.Content = _negativeOffset ? "-" : "+";
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -84,28 +89,29 @@ namespace SRT_to_VTT_Converter
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        private uint getOffsetTime()
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            _dlgOpenFile.ShowDialog();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private int GetOffsetTime()
         {
             var h = UInt32.Parse(TbHours.Text);
             var m = UInt32.Parse(TbMinutes.Text);
             var s = UInt32.Parse(TbSeconds.Text);
             var ms = UInt32.Parse(TbMilliseconds.Text);
 
-            return h * 3600000 + m * 60000 + s * 1000 + ms;
-        }
+            var offset = h * 3600000 + m * 60000 + s * 1000 + ms;
 
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        private void OpenFile(object sender, RoutedEventArgs e)
-        {
-            // Simply show the open file dialog
-            _dlgOpenFile.ShowDialog();
+            return Convert.ToInt32(_negativeOffset ? -offset : offset);
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         // Is called when the open file dialog is closed with a legal file being selected
-        private void dlgOpenFile_FileOk(object sender, CancelEventArgs e)
+        private void DlgOpenFile_FileOk(object sender, CancelEventArgs e)
         {
             // Set up GUI for conversion
             BtnOpenFile.Visibility = Visibility.Collapsed;
@@ -116,7 +122,7 @@ namespace SRT_to_VTT_Converter
             TxtOutput.Visibility = Visibility.Visible;
 
             // Record the offset before starting the conversion
-            _offsetMs = getOffsetTime();
+            _offsetMilliseconds = GetOffsetTime();
 
             // Run the BackgroundWorker asynchronously to convert the selected files
             _backgroundWorker.RunWorkerAsync();
@@ -133,43 +139,34 @@ namespace SRT_to_VTT_Converter
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        private void BtnOffsetPlusMinus_Click(object sender, RoutedEventArgs e)
-        {
-            _nOffsetDirection *= -1; // Toggle the offset direction
-            BtnOffsetPlusMinus.Content = _nOffsetDirection == 1 ? "+" : "-"; // Set the symbol based on the direction
-        }
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        // This event handler is called by running the background worker asynchronously
+        // Async work handler, used to convert each of the user's selected files
         private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
-            int nConverted = 0; // This will count the number of conversions completed
-
-            // For each of the user's selected files
-            foreach (var sFile in _dlgOpenFile.FileNames)
+            int numFilesProcessed = 0;
+            
+            foreach (var filePath in _dlgOpenFile.FileNames)
             {
-                // If the user has requested to cancel the conversion, set the Cancel flag and break out of the loop
-                if (_backgroundWorker.CancellationPending)
+                if (_backgroundWorker.CancellationPending) // The user requested to cancel the conversion
                 {
                     e.Cancel = true;
                     break;
                 }
-
-                //Convert the file and report progress when finished
-                string sDoneMsg = "Done";
+                
+                string doneMsg = "Done";
                 try
                 {
-                    Convert(sFile);
+                    SubtitleConverter.ConvertSRTToVTT(filePath, _offsetMilliseconds);
                 }
                 catch (Exception ex)
                 {
-                    sDoneMsg = "ERROR:\n" + ex.Message;
+                    doneMsg = "ERROR:\n" + ex.Message;
                 }
-                ++nConverted;
+
+                ++numFilesProcessed;
+
                 _backgroundWorker.ReportProgress(
-                    (int)(nConverted / (double)_dlgOpenFile.FileNames.Length * 100), // % complete
-                    nConverted + ". \"" + Path.GetFileName(sFile) + "\" - " + sDoneMsg + "\n" // Done message for the file
+                    (int)(numFilesProcessed / (double)_dlgOpenFile.FileNames.Length * 100),
+                    numFilesProcessed + ". \"" + Path.GetFileName(filePath) + "\" - " + doneMsg + "\n"
                 );
             }
         }
@@ -180,85 +177,37 @@ namespace SRT_to_VTT_Converter
         private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
             LblProgress.Content = "Progress: " + e.ProgressPercentage + "%";
-            TxtOutput.AppendText((string)e.UserState); // Append the message to the output textbox
+            TxtOutput.AppendText((string)e.UserState); // Append the progress message
             TxtOutput.ScrollToEnd(); // Scroll to the bottom to keep the most recently converted files visible
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        //This event handler deals with the results of the background operation
+        // This event handler deals with the results of the background operation
         private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
+            TxtOutput.AppendText("\n");
+
             if (e.Cancelled)
             {
-                TxtOutput.AppendText("PROCESS CANCELLED");
+                TxtOutput.AppendText("Process Cancelled 🚫");
                 BtnCancel.Content = "Cancel";
                 BtnCancel.IsEnabled = true;
             }
             else if (e.Error != null)
             {
                 TxtOutput.AppendText(
-                    "\nERROR!\nThe following error occured during the conversion:\n\n" + e.Error.Message
+                    "ERROR!\nThe following error occured during the conversion:\n\n" + e.Error.Message
                 );
+            }
+            else
+            {
+                TxtOutput.AppendText("Success ✔️");
             }
 
             BtnOpenFile.Visibility = Visibility.Visible;
             BtnCancel.Visibility = Visibility.Collapsed;
             WpOffsetInput.IsEnabled = true;
-        }
-
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-        private void Convert(string sFilePath)
-        {
-            using (var strReader = new StreamReader(sFilePath))
-            using (var strWriter = new StreamWriter(sFilePath.Replace(".srt", ".vtt")))
-            {
-                var rgxDialogNumber = new Regex(@"^\d+$");
-                var rgxTimeFrame = new Regex(@"(\d\d:\d\d:\d\d,\d\d\d) --> (\d\d:\d\d:\d\d,\d\d\d)");
-
-                // Write starting line for the WebVTT file
-                strWriter.WriteLine("WEBVTT");
-                strWriter.WriteLine("");
-
-                // Handle each line of the SRT file
-                string sLine;
-                while ((sLine = strReader.ReadLine()) != null)
-                {
-                    // We only care about lines that aren't just an integer (aka ignore dialog id number lines)
-                    if (rgxDialogNumber.IsMatch(sLine))
-                        continue;
-
-                    // If the line is a time frame line, reformat and output the time frame
-                    Match match = rgxTimeFrame.Match(sLine);
-                    if (match.Success)
-                    {
-                        if (_offsetMs > 0)
-                        {
-                            // Extract the times from the matched time frame line
-                            var tsStartTime = TimeSpan.Parse(match.Groups[1].Value.Replace(',', '.'));
-                            var tsEndTime = TimeSpan.Parse(match.Groups[2].Value.Replace(',', '.'));
-
-                            // Modify the time with the offset
-                            long startTimeMs = _nOffsetDirection * _offsetMs + (uint)tsStartTime.TotalMilliseconds;
-                            long endTimeMs = _nOffsetDirection * _offsetMs + (uint)tsEndTime.TotalMilliseconds;
-                            tsStartTime = TimeSpan.FromMilliseconds(startTimeMs < 0 ? 0 : startTimeMs);
-                            tsEndTime = TimeSpan.FromMilliseconds(endTimeMs < 0 ? 0 : endTimeMs);
-
-                            // Construct the new time frame line
-                            sLine = tsStartTime.ToString(@"hh\:mm\:ss\.fff") +
-                                    " --> " +
-                                    tsEndTime.ToString(@"hh\:mm\:ss\.fff");
-                        }
-                        else
-                        {
-                            sLine = sLine.Replace(',', '.'); // Simply replace the comma in the time with a period
-                        }
-                    }
-
-                    strWriter.WriteLine(sLine); // Write out the line
-                }
-            }
         }
 
     }
---
+++ SRT to VTT Converter/SubtitleConverter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SRT_to_VTT_Converter
+{
+    class SubtitleConverter
+    {
+        private static readonly Regex _rgxDialogNumber = new Regex(@"^\d+$");
+        private static readonly Regex _rgxTimeFrame = new Regex(@"(\d\d:\d\d:\d\d,\d\d\d) --> (\d\d:\d\d:\d\d,\d\d\d)");
+
+        public static void ConvertSRTToVTT(string filePath, int offsetMilliseconds)
+        {
+            using (var srtReader = new StreamReader(filePath))
+            using (var vttWriter = new StreamWriter(filePath.Replace(".srt", ".vtt")))
+            {
+                // Write starting line for the WebVTT file
+                vttWriter.WriteLine("WEBVTT");
+                vttWriter.WriteLine("");
+
+                // Handle each line of the SRT file
+                string srtLine;
+                while ((srtLine = srtReader.ReadLine()) != null)
+                {
+                    if (_rgxDialogNumber.IsMatch(srtLine)) // Ignore dialog ID number lines
+                    {
+                        continue;
+                    }
+
+                    // If the line is a time frame line, reformat and output the time frame
+                    Match match = _rgxTimeFrame.Match(srtLine);
+                    if (match.Success)
+                    {
+                        if (offsetMilliseconds != 0)
+                        {
+                            // Extract the times from the matched time frame line
+                            var startTime = TimeSpan.Parse(match.Groups[1].Value.Replace(',', '.'));
+                            var endTime = TimeSpan.Parse(match.Groups[2].Value.Replace(',', '.'));
+
+                            // Modify the time with the offset
+                            long startTimeMs = (uint)startTime.TotalMilliseconds + offsetMilliseconds;
+                            long endTimeMs = (uint)endTime.TotalMilliseconds + offsetMilliseconds;
+
+                            startTime = TimeSpan.FromMilliseconds(startTimeMs < 0 ? 0 : startTimeMs);
+                            endTime = TimeSpan.FromMilliseconds(endTimeMs < 0 ? 0 : endTimeMs);
+
+                            // Construct the new time frame line
+                            srtLine =
+                                startTime.ToString(@"hh\:mm\:ss\.fff") +
+                                " --> " +
+                                endTime.ToString(@"hh\:mm\:ss\.fff");
+                        }
+                        else
+                        {
+                            srtLine = srtLine.Replace(',', '.'); // Simply replace the comma in the time with a period
+                        }
+                    }
+
+                    vttWriter.WriteLine(srtLine); // Write out the line
+                }
+            }
+        }
+    }
+}
